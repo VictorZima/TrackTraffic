ce484716ae30d3fc4f3a7480d3b95e80a7f21c47
VictorZima <victorzima@icloud.com>
2024-04-23 02:20:06 +0300

    Initial Commit

---
+++ TrackTraffic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrackTraffic
+//
+//  Created by VictorZima on 23/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrackTraffic/TrackTrafficApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackTrafficApp.swift
+//  TrackTraffic
+//
+//  Created by VictorZima on 23/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct TrackTrafficApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
